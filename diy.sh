b2f35156d7ae3f66ccd6f77ffc73830abc1e4895
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-03-31 18:38:16 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,7 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/opentomcat"' package/lean/default-settings/files/zzz-default-settings
+sed -i '/exit/i\chown -R root:root /usr/share/netdata/web' package/lean/default-settings/files/zzz-default-settings
+#sed -i '/8.8.8.8/d' package/base-files/files/root/setup.sh
+echo -e '\nQuintus Build\n'  >> package/base-files/files/etc/banner
