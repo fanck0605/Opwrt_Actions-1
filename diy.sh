6a7a337fefbf53a639d706d178706730b5dfac39
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-03-31 18:52:29 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,3 +11,6 @@ sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/opentomc
 sed -i '/exit/i\chown -R root:root /usr/share/netdata/web' package/lean/default-settings/files/zzz-default-settings
 #sed -i '/8.8.8.8/d' package/base-files/files/root/setup.sh
 echo -e '\nQuintus Build\n'  >> package/base-files/files/etc/banner
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
+git clone https://github.com/kuoruan/luci-app-frpc package/lean/luci-app-frpc
+git clone https://github.com/lwz322/luci-app-frps.git package/lean/luci-app-frps
