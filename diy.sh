b35dcb991996df792e0dc625d51770b9b9c2dc13
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-03-31 19:26:50 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,15 +5,16 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/opentomcat"' package/lean/default-settings/files/zzz-default-settings
-sed -i '/exit/i\chown -R root:root /usr/share/netdata/web' package/lean/default-settings/files/zzz-default-settings
-#sed -i '/8.8.8.8/d' package/base-files/files/root/setup.sh
-echo -e '\nQuintus Build\n'  >> package/base-files/files/etc/banner
 rm -rf package/lean/luci-theme-opentomcat
 rm -rf package/lean/luci-app-frps
 rm -rf package/lean/luci-app-frpc
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
 git clone https://github.com/kuoruan/luci-app-frpc package/lean/luci-app-frpc
 git clone https://github.com/lwz322/luci-app-frps.git package/lean/luci-app-frps
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/opentomcat"' package/lean/default-settings/files/zzz-default-settings
+sed -i '/exit/i\chown -R root:root /usr/share/netdata/web' package/lean/default-settings/files/zzz-default-settings
+#sed -i '/8.8.8.8/d' package/base-files/files/root/setup.sh
+echo -e '\nQuintus Build\n'  >> package/base-files/files/etc/banner
+
