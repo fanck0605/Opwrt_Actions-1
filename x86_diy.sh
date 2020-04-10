35b5e030c8a96f94c6fc167273e6d692582eca29
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-04-11 00:49:16 +0800

    Update x86_diy.sh

--- x86_diy.sh
+++ x86_diy.sh
@@ -17,7 +17,7 @@ rm -rf package/lean/luci-app-frpc
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
 git clone https://github.com/kuoruan/luci-app-frpc.git package/lean/luci-app-frpc
 git clone https://github.com/lwz322/luci-app-frps.git package/lean/luci-app-frps
-git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+git clone https://github.com/lisaac/luci-app-dockerman.git package/lean/luci-app-dockerman
 #set default theme
 sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/opentomcat"' package/lean/default-settings/files/zzz-default-settings
 #set netdata privilege
