117cb2e4a5a61c76fa772a3cc98066d41aba9750
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-04-11 05:40:43 +0800

    Update patch_kernel_5.4.sh

--- r2s/patch_kernel_5.4.sh
+++ r2s/patch_kernel_5.4.sh
@@ -13,5 +13,7 @@ rm target/linux/generic/hack-5.4/700-swconfig_switch_drivers.patch
 ./scripts/patch-kernel.sh ../kernel target/linux/generic/hack-5.4
 cd ../
 wget https://github.com/torvalds/linux/raw/master/scripts/kconfig/merge_config.sh && chmod +x merge_config.sh
-grep -i '_NETFILTER_\|FLOW' ../r2s/.config.override > .config.override
+grep -i '_NETFILTER_\|FLOW' ../.config.override > .config.override
 ./merge_config.sh -m .config.override kernel/arch/arm64/configs/nanopi-r2_linux_defconfig && mv .config kernel/arch/arm64/configs/nanopi-r2_linux_defconfig
+
+sed -i -r 's/# (CONFIG_.*_ERRATUM_.*?) is.*/\1=y/g' kernel/arch/arm64/configs/nanopi-r2_linux_defconfig
