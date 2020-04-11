2cda330d99106fe2466d4b25f99d8bb7410ffbe7
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-04-11 20:20:40 +0800

    Update r2s_diy.sh

--- r2s_diy.sh
+++ r2s_diy.sh
@@ -3,15 +3,17 @@
 sed -i 's/OpenWrt/AutoBuild $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
 #sed -i 's/R20.4.8/& Mod by Quintus/g' package/lean/default-settings/files/zzz-default-settings
 echo -e '\nQuintus Build\n'  >> package/base-files/files/etc/banner
-#移除不用软件包    
-rm -rf package/lean/luci-app-dockerman
+#更新软件包    
 rm -rf package/lean/luci-theme-opentomcat
 rm -rf package/lean/luci-app-frps
 rm -rf package/lean/luci-app-frpc
+rm -rf package/lean/luci-app-dockerman
+rm -rf package/lean/luci-app-diskman
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
 git clone https://github.com/kuoruan/luci-app-frpc.git package/lean/luci-app-frpc
 git clone https://github.com/lwz322/luci-app-frps.git package/lean/luci-app-frps
 git clone https://github.com/lisaac/luci-app-dockerman.git package/lean/luci-app-dockerman
+git clone https://github.com/lisaac/luci-app-diskman.git package/lean/luci-app-diskman
 sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/opentomcat"' package/lean/default-settings/files/zzz-default-settings
 sed -i '/exit/i\chown -R root:root /usr/share/netdata/web' package/lean/default-settings/files/zzz-default-settings
 sed -i '/exit/i\find /etc/rc.d/ -name *docker* -delete' package/lean/default-settings/files/zzz-default-settings
