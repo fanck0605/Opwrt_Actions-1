8642dc538ce509d0fa171bb3dc0cda724e982cbf
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-04-12 12:40:57 +0800

    Update r2s_diy.sh

--- r2s_diy.sh
+++ r2s_diy.sh
@@ -18,7 +18,6 @@ sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/opentomc
 sed -i '/exit/i\chown -R root:root /usr/share/netdata/web' package/lean/default-settings/files/zzz-default-settings
 sed -i '/exit/i\find /etc/rc.d/ -name *docker* -delete' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/option fullcone\t1/option fullcone\t0/' package/network/config/firewall/files/firewall.config
-echo -e '\nMod by Quintus\n'  >> package/base-files/files/etc/banner
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #sed -i '/8.8.8.8/d' package/base-files/files/root/setup.sh
