8cb6b5966aa80cc97deb99c6554bfda342b76681
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-04-13 20:05:26 +0800

    Update freshupdate.sh

--- r2s/freshupdate.sh
+++ r2s/freshupdate.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+cd /tmp/upload/
 unzip /tmp/upload/R2S*.zip
-pv /tmp/upload/*.img.gz | gunzip -dc > /dev/mmcblk0
-echo 1 > /proc/sys/kernel/sysrq
-echo b > /proc/sysrq-trigger
+if [ -f /tmp/upload/Friendly*.img.gz ]; then
+	echo -e '\e[92m开始刷机，请稍候……\e[0m'	
+    pv /tmp/upload/*.img.gz | gunzip -dc > /dev/mmcblk0
+    echo 1 > /proc/sys/kernel/sysrq
+	echo -e '\e[92m刷机完毕，正在重启...稍等片刻后重新登录\e[0m'	
+	echo b > /proc/sysrq-trigger
+fi
