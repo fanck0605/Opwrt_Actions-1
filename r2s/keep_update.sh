a8c92cc6b99735b88e6adef46c3353d827aaa2b1
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-04-13 21:16:34 +0800

    Update keep_update.sh

--- r2s/keep_update.sh
+++ r2s/keep_update.sh
@@ -2,8 +2,8 @@
 cd /mnt/mmcblk0p2
 rm -rf artifact R2S*.zip FriendlyWrt*img*
 mv /tmp/upload/R2S*.zip ./
-unzip R2S*.zip
-rm R2S*.zip
+unzip ./R2S*.zip
+rm ./R2S*.zip
 mv */*.img.gz ./
 if [ -f /mnt/mmcblk0p2/FriendlyWrt*.img.gz ]; then
 	pv /mnt/mmcblk0p2/FriendlyWrt*.img.gz | gunzip -dc > FriendlyWrt.img
