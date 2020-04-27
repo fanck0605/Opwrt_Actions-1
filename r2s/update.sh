c2c0a208eb209c46b74faa95e054c08ef3616004
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-04-28 03:38:25 +0800

    Update update.sh

--- r2s/update.sh
+++ r2s/update.sh
@@ -84,18 +84,24 @@ while [ $checknet -eq 0 ]
 	do
 		echo
 		echo "...........欢迎使用 R2S 一键升级脚本.........."
-		echo " 1. 写入防掉线脚本（开机自动运行）"
+		echo " 防断线脚本是为了处理pppoe模式下网络断连和防火墙崩溃的折中解决方案，若没上述问题，请谨慎刷入"
+		echo		
+		echo " 1. 写入防掉线脚本并于凌晨4点重启网络（开机自动运行）"
 		echo
-		echo " 2. 不写入"
+		echo " 2. 写入防掉线脚本（开机自动运行）"
+		echo
+		echo " 3. 不写入"
 		echo
 		echo
-		read -p "$(echo -e "请选择 [\e[95m1-2\e[0m]，默认为1:")" checknet
-		[[ -z $checknet ]] && mode="1"
+		read -p "$(echo -e "请选择 [\e[95m1-2\e[0m]，默认为3:")" checknet
+		[[ -z $checknet ]] && mode="3"
 		case $checknet in
 		1)
 			checknet=1;;
 		2)
 			checknet=2;;
+		3)
+			checknet=3;;
 		*)
 			checknet=0
 			echo
@@ -258,7 +264,7 @@ if [ $rom -eq 5 ]; then	#上传本地rom
 		mv /tmp/upload/R2S*.zip /mnt/mmcblk0p2/
 	elif [ -f /tmp/upload/Friendly*.img.gz ]; then	 #检测upload目录是否有img.gz升级文件
 		suffix=2
-		mv /tmp/upload/Friendly*.zip /mnt/mmcblk0p2/FriendlyWrt-ROM.img.gz
+		mv /tmp/upload/Friendly*.img.gz /mnt/mmcblk0p2/FriendlyWrt-ROM.img.gz
 	elif [ -f /mnt/mmcblk0p2/R2S*.zip ]; then  #检测upload目录是否有升级文件
 		echo -e '\e[92m找到本地固件，准备解压\e[0m'
 	else
@@ -315,10 +321,16 @@ elif [ -f /bin/update.sh ]; then
 	cp	/bin/update.sh /mnt/img/bin/
 	echo -e '\e[92m写入升级脚本\e[0m'
 fi
-if [ $checknet -eq 1 ]; then   #写入防掉线脚本（开机启动）
+if [ $checknet -le 2 ]; then   #写入防掉线脚本
 	wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/checkwan.sh -O /mnt/img/bin/checkwan.sh
 	chmod +x /mnt/img/bin/checkwan.sh
-	wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/check  -O /etc/init.d/check
+	if [ $checknet -eq 1 ]; then   #写入防掉线脚本并凌晨四点重启网络（开机启动）
+		wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/restartnetwork.sh -O /mnt/img/bin/restartnetwork.sh
+		chmod +x /mnt/img/bin/restartnetwork.sh
+		wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/check2  -O /etc/init.d/check
+	else
+		wget -nv https://github.com/lsl330/R2S-SCRIPTS/raw/master/check  -O /etc/init.d/check
+	fi
 	chmod 777 /etc/init.d/check
 	ln -s /etc/init.d/check /etc/rc.d/S95check
 	cp	/etc/init.d/check /mnt/img/etc/init.d/check
