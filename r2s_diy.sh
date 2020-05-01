7047396649465435c9eb7e26a4c9ec20b2a71f3d
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-05-02 03:58:19 +0800

    fix diy app package

--- r2s_diy.sh
+++ r2s_diy.sh
@@ -5,11 +5,13 @@ echo -e '\nQuintus Build\n'  >> package/base-files/files/etc/banner
 #更新替换软件包    
 rm -rf package/lean/luci-theme-opentomcat
 rm -rf package/lean/luci-app-frpc
+rm -rf package/lean/luci-app-frps
 rm -rf package/lean/luci-app-dockerman
 #rm -rf package/lean/luci-app-zerotier
 #git clone https://github.com/rufengsuixing/luci-app-zerotier.git package/lean/luci-app-zerotier
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
 git clone https://github.com/lwz322/luci-app-frps.git package/lean/luci-app-frps
+git clone https://github.com/kuoruan/luci-app-frpc.git package/lean/luci-app-frpc
 git clone https://github.com/lisaac/luci-app-dockerman.git package/lean/luci-app-dockerman
 
 #更改默認主題
