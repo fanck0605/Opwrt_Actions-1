b1dc5b5693ec9f4f65cc44551aa388af5054ec3c
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-05-08 01:36:05 +0800

    test new config

---
+++ r2s_step/02_rebase2openwrt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd friendlywrt-rk3328/friendlywrt/
+git config --local user.email "action@github.com" && git config --local user.name "GitHub Action"
+git remote add upstream https://github.com/openwrt/openwrt.git && git fetch upstream
+git rebase upstream/openwrt-19.07
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+wget https://raw.githubusercontent.com/openwrt/openwrt/openwrt-19.07/feeds.conf.default
