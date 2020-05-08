7419e6db270934f651029514d81cbd38bf06236d
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-05-08 12:45:00 +0800

    test rebase to kernel 5.4

--- r2s_step/02_rebase5.4.sh
+++ r2s_step/02_rebase5.4.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd friendlywrt-rk3328/kernel
 clear
 git remote add upstream https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git && git fetch upstream
 git rebase upstream/linux-5.4.y
