fb473352f499ccb342bba3ba21fd426a72e4ade4
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-05-08 09:02:02 +0800

    test new config

---
+++ r2s_step/02_rebase5.4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clear
+git remote add upstream https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git && git fetch upstream
+git rebase upstream/linux-5.4.y
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+exit 0
