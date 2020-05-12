3bd8d878e4277347c84a7c7cfd7f84e034e31ad7
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-05-12 11:57:57 +0800

    test test

--- test/06_mod_test.sh
+++ test/06_mod_test.sh
@@ -12,8 +12,6 @@ mv ../../script/update.sh package/base-files/files/root/update.sh && chmod +x pa
 sed -i 's/OpenWrt/Quintus Build @ $(date "+%Y.%m.%d")/g' package/lean/default-settings/files/zzz-default-settings
 echo -e '\nQuintus Build\n'  >> package/base-files/files/etc/banner
 #生成时间
-VersionDate=$(git show -s --date=short --format="date:%cd")
-echo "::set-env name=VersionDate::$VersionDate"
 echo "::set-env name=DATE::$(date "+%Y-%m-%d %H:%M:%S")"
 #更新替换软件包
 rm -rf package/lean/luci-theme-opentomcat
@@ -39,11 +37,11 @@ sed -i 's/luci.main.lang=zh_cn/luci.main.lang=auto/g' package/lean/default-setti
 #sed -i '/uci commit luci/a\uci commit dropbear' package/lean/default-settings/files/zzz-default-settings
 #sed -i '/uci commit luci/a\uci set dropbear.@dropbear[0].Interface='lan'' package/lean/default-settings/files/zzz-default-settings
 #关闭ipv6
-sed -i '/uci commit/i\uci delete network.lan.ip6assign' package/base-files/files/root/setup.sh
-sed -i '/uci commit/i\uci delete network.wan6' package/base-files/files/root/setup.sh
-sed -i '/uci commit/i\uci delete dhcp.lan.ra' package/base-files/files/root/setup.sh
-sed -i '/uci commit/i\uci delete dhcp.lan.dhcpv6' package/base-files/files/root/setup.sh
-sed -i '/uci commit/i\uci delete dhcp.lan.ndp' package/base-files/files/root/setup.sh
+#sed -i '/uci commit/i\uci delete network.lan.ip6assign' package/base-files/files/root/setup.sh
+#sed -i '/uci commit/i\uci delete network.wan6' package/base-files/files/root/setup.sh
+#sed -i '/uci commit/i\uci delete dhcp.lan.ra' package/base-files/files/root/setup.sh
+#sed -i '/uci commit/i\uci delete dhcp.lan.dhcpv6' package/base-files/files/root/setup.sh
+#sed -i '/uci commit/i\uci delete dhcp.lan.ndp' package/base-files/files/root/setup.sh
 #默认dnsmasq-full
 #sed -i 's/dnsmasq /dnsmasq-full default-settings luci /' include/target.mk
 #增加最大连接
