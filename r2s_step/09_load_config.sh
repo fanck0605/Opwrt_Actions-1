a06146bb274972c0c9ad21076eee5a6b2b6e460e
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-05-14 02:15:18 +0800

    test O3

--- r2s_step/09_load_config.sh
+++ r2s_step/09_load_config.sh
@@ -2,11 +2,12 @@
 export TERM=linux
 cd friendlywrt-rk3328
 #deconfig
-#wget https://github.com/torvalds/linux/raw/master/scripts/kconfig/merge_config.sh && chmod +x merge_config.sh
-#./merge_config.sh -m kernel/arch/arm64/configs/nanopi-r2_linux_defconfig ../seed/defconfig.override
-#cat .config > kernel/arch/arm64/configs/nanopi-r2_linux_defconfig
 wget https://github.com/fanck0605/nanopi-r2s/raw/lean/nanopi-r2_linux_defconfig
 cat ./nanopi-r2_linux_defconfig > ./kernel/arch/arm64/configs/nanopi-r2_linux_defconfig
+echo '
+CONFIG_CC_DISABLE_WARN_MAYBE_UNINITIALIZED=y
+CONFIG_CC_OPTIMIZE_FOR_PERFORMANCE_O3=y
+' >> ./kernel/arch/arm64/configs/nanopi-r2_linux_defconfig
 #.config
 rm -f friendlywrt/.config*
 cat configs/config_rk3328 | grep "TARGET" >> ../seed/base_rk3328.seed
