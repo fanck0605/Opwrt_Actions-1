4fcf134d6fe6f68b5137b719446ebce1789e6e94
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-05-13 12:55:05 +0800

    for test

--- test/01_friendlywrt.sh
+++ test/01_friendlywrt.sh
@@ -3,7 +3,7 @@ export TERM=linux
 git clone https://github.com/friendlyarm/repo
 sudo cp repo/repo /usr/bin/
 rm -rf friendlywrt-rk3328
-mkdir friendlywrt-rk3328 && cd friendlywrt-rk3328
+mkdir $RK3328_DIR/ && cd $RK3328_DIR/
 repo init -u https://github.com/friendlyarm/friendlywrt_manifests -b master-v19.07.1 -m rk3328.xml --repo-url=https://github.com/friendlyarm/repo --no-clone-bundle
 repo sync -c --no-tags --no-clone-bundle -j8
 exit 0
