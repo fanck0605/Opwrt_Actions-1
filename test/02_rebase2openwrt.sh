4dce894187e5561f2083c5fb720d244c968b80f8
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-05-13 13:13:44 +0800

    for test

--- test/02_rebase2openwrt.sh
+++ test/02_rebase2openwrt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #调整friendlywrt的更新源，#fork 自 QiuSimons
 export TERM=linux
+cd $RK3328_DIR/friendlywrt/
 latest_feed="$(curl -s https://github.com/openwrt/openwrt/releases |grep -Eo "v[0-9\.]+.tar.gz" |sed -n 1p |sed 's/v//g' |sed 's/.tar.gz//g')"
 sed -i 's,19.07.1,'"${latest_feed}"',g' device/friendlyelec/rk3328/common-files/etc/opkg/distfeeds.conf
 #取消重复的feeds update 
 sed -i 's,./scripts,#./scripts,g' scripts/mk-friendlywrt.sh
 #rebase to openwrt
-cd $RK3328_DIR/friendlywrt/
 git config --local user.email "action@github.com" && git config --local user.name "GitHub Action"
 git remote add upstream https://github.com/openwrt/openwrt.git && git fetch upstream
 git rebase upstream/openwrt-19.07
