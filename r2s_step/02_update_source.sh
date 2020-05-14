a27e11e14a13efa1f5112bab2c14c28c7e8f3ffe
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-05-15 02:11:49 +0800

    test tiny with new rebase lean

--- r2s_step/02_update_source.sh
+++ r2s_step/02_update_source.sh
@@ -4,6 +4,16 @@ cd friendlywrt-rk3328/friendlywrt/
 git config --local user.email "action@github.com" && git config --local user.name "GitHub Action"
 git remote add lean https://github.com/coolsnowwolf/lede.git
 git fetch lean && git rebase lean/master
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
+git rebase --skip
 sed -i '$a\src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
 sed -i 's/^src-git telephony/#src-git telephony/g' ./feeds.conf.default
 exit 0
