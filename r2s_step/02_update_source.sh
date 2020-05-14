57d9d4eaf93ad3ce35cba216c8685d1a83585cb0
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-05-14 18:45:18 +0800

    test tiny config

--- r2s_step/02_update_source.sh
+++ r2s_step/02_update_source.sh
@@ -6,7 +6,4 @@ git remote add lean https://github.com/coolsnowwolf/lede.git
 git fetch lean && git rebase lean/master
 sed -i '$a\src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
 sed -i 's/^src-git telephony/#src-git telephony/g' ./feeds.conf.default
-cd ../kernel
-git remote add linux https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
-git fetch linux && git rebase linux/linux-5.4.y
 exit 0
