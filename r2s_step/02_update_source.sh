9b4ccb14eacccb983389949273ab86556f67d413
ardanzhu <31897806+ardanzhu@users.noreply.github.com>
2020-05-15 12:00:47 +0800

    Update 02_update_source.sh

--- r2s_step/02_update_source.sh
+++ r2s_step/02_update_source.sh
@@ -45,4 +45,5 @@ git rebase --skip
 git rebase --skip
 sed -i '$a\src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
 sed -i 's/^src-git telephony/#src-git telephony/g' ./feeds.conf.default
+rm target/linux/rockchip-rk3328/patches-4.14/0001-net-thunderx-workaround-BGX-TX-Underflow-issue.patch
 exit 0
